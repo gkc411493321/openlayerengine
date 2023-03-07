4e0b195cbe2c9443fcceb303bf62cf1fd94e7b07
guankecheng <411493321@qq.com>
2023-03-07 16:57:53 +0800

    新增鼠标点击事件：全局/模块

--- .test/common/GlobalEvent.ts
+++ .test/common/GlobalEvent.ts
@@ -1,8 +1,11 @@
+import Interaction from "ol/interaction/Interaction"
+import PointerInteraction from 'ol/interaction/Pointer.js';
+import { unByKey } from "ol/Observable";
 import { useEarth } from "../../src"
 
 export const testGlobalEvent = () => {
   /**
-   * 启用模块鼠标监听
+   * 启用模块鼠标移动监听
    */
   // useEarth().useGlobalEvent().enableModuleMouseMoveEvent();
   // useEarth().useGlobalEvent().addMouseMoveEventByModule("circle", (param) => {
@@ -10,11 +13,45 @@ export const testGlobalEvent = () => {
   // })
   // useEarth().useGlobalEvent().disableModuleMouseMoveEvent();
   /**
-   * 启用全局鼠标监听
+   * 启用全局鼠标移动监听
    */
   // useEarth().useGlobalEvent().enableGlobalMouseMoveEvent();
   // useEarth().useGlobalEvent().addMouseMoveEventByGlobal((param) => {
   //   console.log(param)
   // })
-  // useEarth().useGlobalEvent().disableGlobalMouseMoveEvent()
+  // useEarth().useGlobalEvent().disableGlobalMouseMoveEvent();
+  /**
+   * 启用模块鼠标点击监听
+   */
+  // useEarth().useGlobalEvent().enableModuleMouseClickEvent();
+  // useEarth().useGlobalEvent().addMouseClickEventByModule("point", (param) => {
+  //   console.log(param)
+  // })
+  // setTimeout(() => {
+  //   useEarth().useGlobalEvent().disableModuleMouseClickEvent();
+  // }, 5000)
+  // useEarth().map.getViewport().onclick = (e) => {
+  //   console.log(e, "click")
+  // }
+  // useEarth().map.getViewport().onmousedown = (e) => {
+  //   console.log(e, "down")
+  // }
+  // useEarth().map.getViewport().onmouseup = (e) => {
+  //   console.log(e, "up")
+  // }
+  const test = (e: MouseEvent) => {
+    e.preventDefault();
+    let pixel = useEarth().map.getEventPixel({ clientX: e.x, clientY: e.y });
+    let feature = useEarth().map.forEachFeatureAtPixel(pixel, function (feature) {
+      return feature;
+    })
+    if (feature) {
+      let coordinate = useEarth().map.getEventCoordinate(e);
+      console.log(feature, coordinate)
+    }
+  }
+  useEarth().map.getViewport().addEventListener("contextmenu", test)
+  // setTimeout(() => {
+  //   useEarth().map.getViewport().removeEventListener("contextmenu", test)
+  // }, 3000)
 }
--- src/commponents/GlobalEvent.ts
+++ src/commponents/GlobalEvent.ts
@@ -33,9 +33,17 @@ export default class GlobalEvent {
    */
   private moduleMouseMoveEvent: Map<string, { callback: ModuleEventCallback }> = new Map();
   /**
+   * 按模块的鼠标点击事件集合 
+   */
+  private moduleMouseClickEvent: Map<string, { callback: ModuleEventCallback }> = new Map();
+  /**
    * 全局的鼠标移动事件
    */
   private globalMouseMoveEvent?: { callback: GlobalEventCallback };
+  /**
+   * 全局的鼠标点击事件
+   */
+  private globalMouseClickEvent?: { callback: GlobalEventCallback };
   /**
    * 构造器
    * @param earth 地图实例 
@@ -44,7 +52,7 @@ export default class GlobalEvent {
     this.map = earth.map;
   }
   /**
-   * 启用模块鼠标移动事件监听
+   * 启用模块下鼠标移动事件监听
    */
   enableModuleMouseMoveEvent(): void {
     if (!this.eventKey?.has("moduleMouseMove")) {
@@ -82,11 +90,42 @@ export default class GlobalEvent {
       })
       this.eventKey?.set("moduleMouseMove", key);
     } else {
-      console.warn("重复启用模块鼠标移动事件监听,请检查");
+      console.warn("重复启用模块下鼠标移动事件监听,请检查");
     }
   }
   /**
-   * 启用全局鼠标移动事件监听
+   * 启用模块下鼠标点击事件监听
+   */
+  enableModuleMouseClickEvent(): void {
+    if (!this.eventKey?.has("moduleMouseClick")) {
+      const key = this.map.on("click", (evt) => {
+        let features = this.map.forEachFeatureAtPixel(evt.pixel, (feature, layer) => {
+          return {
+            id: feature.getId(),
+            module: feature.get("module"),
+            feature: <Feature>feature,
+            layer
+          }
+        })
+        if (features && features.feature.get("module")) {
+          const moduleEvent = this.moduleMouseClickEvent.get(features.feature.get("module"));
+          if (moduleEvent) {
+            moduleEvent.callback.call(this, {
+              position: toLonLat(evt.coordinate),
+              feature: features.feature,
+              layer: features.layer,
+              entityId: features.id
+            })
+          }
+        }
+      })
+      this.eventKey?.set("moduleMouseClick", key);
+    } else {
+      console.warn("重复启用模块下鼠标点击事件监听,请检查");
+    }
+  }
+  /**
+   * 启用全局下鼠标移动事件监听
    */
   enableGlobalMouseMoveEvent(): void {
     if (!this.eventKey?.has("globalMouseMove")) {
@@ -99,7 +138,20 @@ export default class GlobalEvent {
     }
   }
   /**
-   * 停用模块鼠标移动事件监听
+   * 启用全局下鼠标点击事件监听
+   */
+  enableGlobalMouseClickEvent(): void {
+    if (!this.eventKey?.has("globalMouseClick")) {
+      const key = this.map.on("pointermove", (evt) => {
+        this.globalMouseClickEvent?.callback.call(this, evt);
+      })
+      this.eventKey?.set("globalMouseClick", key);
+    } else {
+      console.warn("重复启用全局鼠标点击事件监听,请检查");
+    }
+  }
+  /**
+   * 停用模块下鼠标移动事件监听
    */
   disableModuleMouseMoveEvent(): void {
     const key = this.eventKey?.get("moduleMouseMove");
@@ -108,11 +160,24 @@ export default class GlobalEvent {
       this.eventKey?.delete("moduleMouseMove");
       this.moduleMouseMoveEvent.clear();
     } else {
-      console.warn("未启用模块鼠标移动事件监听，关闭失败");
+      console.warn("未启用模块下鼠标移动事件监听，关闭失败");
     }
   }
   /**
-   * 停用全局鼠标移动事件监听
+   * 停用模块下鼠标移动事件监听
+   */
+  disableModuleMouseClickEvent(): void {
+    const key = this.eventKey?.get("moduleMouseClick");
+    if (key) {
+      unByKey(key);
+      this.eventKey?.delete("moduleMouseClick");
+      this.moduleMouseClickEvent.clear();
+    } else {
+      console.warn("未启用模块下鼠标点击事件监听，关闭失败");
+    }
+  }
+  /**
+   * 停用全局下鼠标移动事件监听
    */
   disableGlobalMouseMoveEvent(): void {
     const key = this.eventKey?.get("globalMouseMove");
@@ -121,7 +186,20 @@ export default class GlobalEvent {
       this.eventKey?.delete("globalMouseMove");
       this.globalMouseMoveEvent = undefined;
     } else {
-      console.warn("未启用全局鼠标移动事件监听，关闭失败");
+      console.warn("未启用全局下鼠标移动事件监听，关闭失败");
+    }
+  }
+  /**
+   * 停用全局下鼠标点击事件监听
+   */
+  disableGlobalMouseClickEvent(): void {
+    const key = this.eventKey?.get("globalMouseClick");
+    if (key) {
+      unByKey(key);
+      this.eventKey?.delete("globalMouseClick");
+      this.globalMouseClickEvent = undefined;
+    } else {
+      console.warn("未启用全局下鼠标点击事件监听，关闭失败");
     }
   }
   /**
@@ -141,10 +219,33 @@ export default class GlobalEvent {
     }
   }
   /**
-   * 全局添加鼠标移动事件
+   * 按模块添加鼠标点击事件
+   * @param module 模块名称
+   * @param callback 回调函数，详见{@link ModuleEventCallback}
+   */
+  addMouseClickEventByModule(module: string, callback: ModuleEventCallback): void {
+    if (module && module !== "") {
+      if (!this.moduleMouseClickEvent.get(module)) {
+        this.moduleMouseClickEvent.set(module, { callback });
+      } else {
+        console.warn('按模块追加全局鼠标点击事件: module参数重复', module);
+      }
+    } else {
+      console.warn('按模块追加全局鼠标点击事件: module参数不能为空');
+    }
+  }
+  /**
+   * 按全局添加鼠标移动事件
    * @param callback 回调函数，需使用ol原生方法调用。详见{@link GlobalEventCallback}
    */
   addMouseMoveEventByGlobal(callback: GlobalEventCallback): void {
     this.globalMouseMoveEvent = { callback }
+  };
+  /**
+   * 按全局添加鼠标点击事件
+   * @param callback 回调函数，需使用ol原生方法调用。详见{@link GlobalEventCallback}
+   */
+  addMouseClickEventByGlobal(callback: GlobalEventCallback): void {
+    this.globalMouseClickEvent = { callback }
   };
 }
